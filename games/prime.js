7c3e205d148a8f04b78cf76650f48492b57813ce
gitlearn07 <rami.412222@yahoo.com>
2025-04-21 15:54:20 +0200

    add brain prime game

---
+++ games/prime.js
@@ -0,0 +1,30 @@
+import runGame from '../src/index.js';
+
+import generateRandomNumber from '../utils/utils.js';
+
+const gameIntroMessage = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isNumberPrime = (number) => {
+  if (number < 2) {
+    return false;
+  }
+
+  for (let i = 2; i < number / 2; i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const getQuestionAndAnswer = () => {
+  const randomNumber = generateRandomNumber(1, 100);
+  const correctAnswer = isNumberPrime(randomNumber) ? 'yes' : 'no';
+
+  const question = `Question: ${randomNumber}`;
+
+  return [question, correctAnswer];
+};
+
+export default () => runGame(gameIntroMessage, getQuestionAndAnswer);
